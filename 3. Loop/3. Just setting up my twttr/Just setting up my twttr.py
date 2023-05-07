157f4c2c1dd1bcf33f4ddf240cf81e0352a20715
Jairo Watts <jwatts.pajaro@gmail.com>
2023-05-07 18:53:16 -0500

    Update Just setting up my twttr.py

--- 3. Loop/3. Just setting up my twttr/Just setting up my twttr.py
+++ 3. Loop/3. Just setting up my twttr/Just setting up my twttr.py
@@ -1,1 +1,18 @@
+# Función para eliminar las vocales de un texto
+def remove_vowels(text):
+    # Define las vocales en una cadena de caracteres
+    vowels = "AEIOUaeiou"
+    # Crea una lista de caracteres que no sean vocales
+    result = [char for char in text if char not in vowels]
+    # Devuelve el texto sin vocales como una cadena
+    return ''.join(result)
+
+# Si el script se ejecuta directamente (no importado como módulo)
+if __name__ == "__main__":
+    # Solicita al usuario que ingrese un texto
+    input_text = input("Ingresa el texto: ")
+    # Elimina las vocales del texto ingresado usando la función remove_vowels
+    output_text = remove_vowels(input_text)
+    # Imprime el texto sin vocales
+    print(output_text)
 
