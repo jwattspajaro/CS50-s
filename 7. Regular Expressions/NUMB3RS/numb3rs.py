bff8bcce4eb7f495958b45ed662b90b706c41116
Jairo Watts <jwatts.pajaro@gmail.com>
2023-05-29 09:13:17 -0500

    Update numb3rs.py

--- 7. Regular Expressions/NUMB3RS/numb3rs.py
+++ 7. Regular Expressions/NUMB3RS/numb3rs.py
@@ -1,3 +1,5 @@
+
+
 import re  # Importamos la biblioteca de expresiones regulares
 
 def main():
@@ -16,3 +18,9 @@ def validate(ip):
         return True
     else:
         return False
+    
+# El código importa el módulo re, que proporciona soporte para expresiones regulares. Define dos funciones: principal y validar.
+# La función principal solicita al usuario que ingrese una dirección IPv4 y luego llama a la función de validación con la dirección ingresada como argumento.
+# Luego se imprime el resultado de la función de validación.
+# La función de validación toma una dirección IPv4 como entrada y usa una expresión regular para validarla.
+# La expresión regular se almacena en la variable regex y representa el patrón para una dirección IPv4 válida
